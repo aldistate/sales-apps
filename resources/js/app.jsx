4e8aa066aeebe7202f38f0c80213ed520af9fe98
abrnsyh <abriansyah1099@gmail.com>
2023-11-28 20:26:38 +0700

    Add sidebar and custom theme (chakra)

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,12 @@
 import './bootstrap';
 import '../css/app.css';
+import '@fontsource/poppins';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './Helper/theme';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,7 +20,11 @@ createInertiaApp({
   setup({ el, App, props }) {
     const root = createRoot(el);
 
-    root.render(<App {...props} />);
+    root.render(
+      <ChakraProvider theme={theme}>
+        <App {...props} />
+      </ChakraProvider>
+    );
   },
   progress: {
     color: '#4B5563',
