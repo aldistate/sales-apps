588071f66c6f844be919b2eeba0bf011bbb27add
AldiState <aldistate@yahoo.com>
2023-12-04 17:21:07 +0700

    refactor migration transaction

---
+++ database/migrations/2023_12_01_020520_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('external_id');
+            $table->bigInteger('fee')->nullable();
+            $table->text('title');
+            $table->text('description');
+            $table->string('status');
+            $table->string('event_name')->nullable();
+            $table->string('event_type')->nullable();
+            $table->string('event_channel')->nullable();
+            $table->bigInteger('amount');
+            $table->string('return_url');
+            $table->timestamp('expires_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
